6114d5282e529a22c918b232790c78aaa0e71c67
Sottier Liam <o22202962@info25-16.iut45.univ-orleans.fr>
2024-03-26 18:09:10 +0100

    app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import Questionnaires from './components/Questionnaires.vue';
+// import Questions from './components/Questions.vue';
+
+let data={
+  title:'Voici vos questionnaires',
+  questions:false,
+  operations:false,
+
+}
+
+
+export default{
+  data(){
+    return data;
+  },
+  components: {Questionnaires},
+  
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+  <div class="questionnaires">
+    <h2>Questionnaires : </h2>
+    <Questionnaires>
+
+    </Questionnaires>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="questions">
+    <h2>Questions : </h2>
+    <!-- <Questions v-if="this.questions"></Questions> -->
+  </div>
+  <div class="operations">
+
+
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
