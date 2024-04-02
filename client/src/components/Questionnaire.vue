a4ac1b2119c28ed6366c2146f70a2bb6a1543656
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2024-04-02 18:30:47 +0200

    Feat/ ajout

--- client/src/components/Questionnaire.vue
+++ client/src/components/Questionnaire.vue
@@ -1,20 +1,36 @@
 <script>
  export default {
    props: {
-     name: String,
-     url: String,
+     questionnaire : Object
    },
-   methodes: {
+   data() {
+     return {
+       modEdit: false
+     };
+   },
+   methods: {
     afficheQuestions : function() {
             this.$emit('afficheQuest', {url: this.url});
-        }
+    },
+    changeMod : function() {
+      this.modEdit = !this.modEdit;
+      console.log(this.modEdit);
+    },
+    editQuestionnaire : function() {
+      emit('editQuest', {name: this.questionnaire.name, id: this.questionnaire.id});
+    }
    },
-   emits: ['afficheQuest']
+   emits: ['afficheQuest', 'editQuest']
  }
 </script>
 
 <template>
-  <div>
-    <h1 @click="afficheQuestions">{{ name }}</h1>
+  <div v-if="!modEdit">
+    <h1 @click="afficheQuestions">{{ questionnaire.name }}</h1>
+    <input type="button" value="Modifier" @click="changeMod">
+  </div>
+  <div v-else>
+    <input type="text" v-model="questionnaire.name">
+    <input type="button" value="Valider" @click="editQuestionnaire">
   </div>
 </template>
--- client/src/components/Questionnaires.vue
+++ client/src/components/Questionnaires.vue
@@ -2,7 +2,8 @@
 export default {
   data() {
     return {
-      questionnaires : []
+      questionnaires : [],
+      newQuestionnaire : ""
     };
   },
   methods: {
@@ -12,12 +13,26 @@ export default {
       .then(data => {
         this.questionnaires = [];
         for (let i = 0; i < data.length; i++) {
-          this.questionnaires.push({name: data[i].name, url: data[i].url});
+          this.questionnaires.push({name: data[i].name, url: data[i].url, id: data[i].id});
         }
       });
     },
     afficheQuestions : function() {
         this.$emit('afficheQuest', {url: this.url});
+    },
+    ajouterQuestionnaire : function() {
+      fetch('http://localhost:5000/quiz/api/v1.0/quiz', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: this.newQuestionnaire})
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.newQuestionnaire = "";
+        this.affichQuestionnaires();
+      });
     }
   },
   emits: ['afficheQuest'],
@@ -32,8 +47,9 @@ import Questionnaire from './Questionnaire.vue';
         <button @click="affichQuestionnaires">Afficher les questionnaires</button>
         <ul>
             <li v-for="quest in questionnaires">
-                <Questionnaire :name="quest.name" :url="quest.url" @afficheQuest="afficheQuestions"></Questionnaire>
+                <Questionnaire :questionnaire="quest" @afficheQuest="afficheQuestions"></Questionnaire>
             </li>
         </ul>
+        <input type="text" v-model="newQuestionnaire" placeholder="Ajouter un nouveau questionnaire" @keyup.enter="ajouterQuestionnaire">
     </section>
 </template>
